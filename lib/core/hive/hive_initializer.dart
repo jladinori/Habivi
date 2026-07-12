9ff95b946308e29f7322191e11ac5795103e0038
FrankFuentes7 <frfuentesa@unal.edu.co>
2026-07-12 16:34:27 -0500

    feat: Register RachaAdapter in HiveInitializer

--- lib/core/hive/hive_initializer.dart
+++ lib/core/hive/hive_initializer.dart
@@ -4,6 +4,7 @@ import 'package:habivi/data/models/cuenta.dart';
 import 'package:habivi/data/models/evento.dart';
 import 'package:habivi/data/models/habito.dart';
 import 'package:habivi/data/models/nota.dart';
+import 'package:habivi/data/models/racha.dart';
 import 'package:habivi/data/models/registro_habito.dart';
 import 'package:habivi/data/models/sesion_estudio.dart';
 import 'package:habivi/data/models/sesion_pomodoro.dart';
@@ -58,6 +59,9 @@ class HiveInitializer {
     }
     if (!Hive.isAdapterRegistered(9)) {
       Hive.registerAdapter(SesionEstudioAdapter());
+    }
+    if (!Hive.isAdapterRegistered(10)) {
+      Hive.registerAdapter(RachaAdapter());
     }
   }
 }
